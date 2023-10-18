446c5e1dbbe62a2a4847f3b5a3df9b6c5607df48
jeremycmdr <115502515+jeremycmdr@users.noreply.github.com>
2023-10-18 20:27:17 +0200

    pages folder move landing page to pages folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import LandingPage from "./components/landingPage/LandingPage";
+import LandingPage from "./pages/landingPage/LandingPage";
 
 function App() {
   return (
